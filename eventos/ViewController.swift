85419b5b4029a00900f3e7b205bea33519dc06b4
Marco Antonio <ulsa@Pol.local>
2019-11-04 14:50:50 -0700

    primero

---
+++ eventos/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  eventos
+//
+//  Created by MaestroD1 on 11/1/19.
+//  Copyright © 2019 MaestroD1. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var eventos : [Evento] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AF.request("http://localhost:8888/eventos/wp-json/v2/eventos").responseJSON{
+            response in
+            switch(response.result){
+            case.success(let datos) :
+                if let arregloEventos = datos as? NSArray {
+                    for evento in arregloEventos{
+                        if let diccionarioEvento = evento as? NSDictionary {
+                            let nuevoEvento = Evento(diccionar: diccionarioEvento)
+                            self.eventos.append(nuevoEvento)
+                        }
+                    }
+                }
+            case.failure(let datos) : print("")
+            default : break
+            }
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "cellEvento") as? CeldaEventoController
+        celda?.lblNombre.text = eventos[indexPath.row].nombre
+        celda?.lblFecha.text = eventos[indexPath.row].fecha
+        
+        return celda!
+    }
+
+
+}
+
